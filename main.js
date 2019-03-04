4a2bb73b53bab2617b266bc75ff24a0b9823dfc2
Chikimuras <alexandre.m.velia@gmail.com>
2019-03-05 00:12:37 +0100

    Finish coding challenge n°2 and start video 3

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var app = new Vue({
     data: {
         product: "Chaussettes",
         description: "De chaudes et confortable chaussettes",
-        image: "greensocks.png"
+        image: "greensocks.png",
+        link: "https://www.google.fr"
     }
 })
